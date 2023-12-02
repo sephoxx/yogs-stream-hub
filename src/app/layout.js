3cbf51f2298943b31cc5369679e4d733f9d678e4
sephoxx <sephoxx@gmail.com>
2023-12-02 13:42:02 +0200

    favicon update

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,12 +2,15 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { PrimeReactProvider } from 'primereact/api';
 import "primereact/resources/themes/md-dark-indigo/theme.css";
-
+import favicon from "/public/favicon.svg";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Yogs Jingle Jam Stream Hub",
+  icons: {
+    icon: favicon.src
+  }
 };
 
 export default function RootLayout({ children }) {
